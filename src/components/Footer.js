b12fb6f3ee4b07f70cd7fee234e70b9c71440093
HuuSang15 <phamhuusang24@gmail.com>
2023-05-21 19:37:41 +0700

    Upload Code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,43 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <footer className="container mx-auto py-12 border ">
+  <div className="flex flex-col md:flex-row text-center md:text-left gap-12 md:gap-8">
+    <div className="basis-1/3">
+      <div className="font-bold text-xl mb-4 text-gray-800">HomeLand.</div>
+      <div className="mb-8 md:mb-20">Delivering the best coffee life since 1996. From coffee geeks to the real ones.</div>
+      <div className="text-gray-400 hover:text-coffee-400">Huu Sang Dev. © 2023</div>
+    </div>
+    <div className="basis-1/6">
+      <div className="uppercase font-semibold tracking-wider text-gray-600 mb-4">Menu</div>
+      <div className="flex flex-col gap-3">
+        <div className=""><a href="/" className="hover:text-violet-500">Home</a></div>
+        <div className=""><a href="/" className="hover:text-violet-500">Our Products</a></div>
+        <div className=""><a href="/" className="hover:text-violet-500">About</a></div>
+        <div className=""><a href="/" className="hover:text-violet-500">Contact</a></div>
+        <div className=""><a href="/" className="hover:text-violet-500">Styleguide</a></div>
+        <div className=""><a href="https://github.com/HuuSang15" target="blank" className="hover:text-violet-500">HuuSangDev</a></div>
+      </div>
+    </div>
+    <div className="basis-1/6">
+      <div className="uppercase font-semibold tracking-wider text-gray-600 mb-4">Folllow Us</div>
+      <div className="flex flex-col gap-3">
+        <div className=""><a href="https://facebook.com/no157" className="hover:text-violet-500">Facebook</a></div>
+        <div className=""><a href="https://github.com/HuuSang15" className="hover:text-violet-500">GitHub</a></div>
+        <div className=""><a href="/" className="hover:text-violet-500">Pinterest</a></div>
+        <div className=""><a href="/" className="hover:text-violet-500">Twitter</a></div>
+      </div>
+    </div>
+    <div className="basis-1/3">
+      <div className="uppercase font-semibold tracking-wider text-gray-600 mb-8 md:mb-4">Contact Us</div>
+      <div className="mb-4">We&apos;re Always Happy to Help</div>
+      <div className="mb-16 text-gray-800 text-[20px] tracking-wide">
+        <a href="mailto:phamhuusang23@gmail.com" className="hover:text-violet-500">phamhuusang24@gmail.com</a>
+      </div>
+      <div className=""><a href="https://github.com/HuuSang15" target="blank" className="hover:text-violet-500">Powered by HuuSang Author</a></div>
+    </div>
+  </div>
+</footer>
 };
 
 export default Footer;
--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -1,7 +1,130 @@
-import React from 'react';
+import React, { useState, useEffect, createContext } from "react";
 
-const HouseContext = () => {
-  return <div>HouseContext</div>;
+//import Data
+import { housesData } from "../data";
+
+// create Context
+export const HouseContext = createContext();
+
+const HouseContextProvider = ({ children }) => {
+  const [houses, setHouses] = useState(housesData);
+  const [country, setCountry] = useState("Location (any)");
+  const [countries, setCountries] = useState([]);
+  const [property, setProperty] = useState("Property type (any)");
+  const [properties, setProperties] = useState([]);
+  const [price, setPrice] = useState("Price range (any)");
+  const [loading, setLoading] = useState(false);
+
+  // return all countries
+  useEffect(() => {
+    const allCountries = houses.map((house) => {
+      return house.country;
+    });
+    // Remove cac ten trung lap
+    const uniqueCountries = ["Location (any)", ...new Set(allCountries)];
+    // Set countries state (Đặt trạng thái quốc gia)
+    setCountries(uniqueCountries);
+  }, []);
+
+  // return all properties
+  useEffect(() => {
+    const allProperties = houses.map((house) => {
+      return house.type;
+    });
+    // Remove cac ten trung lap
+    const uniqueProperties = ["Location (any)", ...new Set(allProperties)];
+    // Set properties state (Đặt trạng thái thuộc tính)
+    setProperties(uniqueProperties);
+  }, []);
+
+  const handleClick = () => {
+    //set Loading
+    setLoading(true);
+
+    // create a function that checks if the string includes '(any)'
+    const isDefault = (str) => {
+      return str.split(" ").includes("(any)");
+    };
+    // get the first value of price part it to number(lấy giá trị đầu tiên của phần giá thành số)
+    const minPrice = parseInt(price.split(" ")[0]);
+    // get the second value which it the maximum price & part it to number(lấy giá trị thứ hai là giá tối đa và chia nó thành số)
+    const maxPrice = parseInt(price.split(" ")[2]);
+
+    const newHouses = housesData.filter((house) => {
+      const housePrice = parseInt(house.price);
+      // if all values selected
+      if (
+        house.country === country &&
+        house.type === property &&
+        housePrice >= minPrice &&
+        housePrice <= maxPrice
+      ) {
+        return house;
+      }
+      // if all values default
+      if (isDefault(country) && isDefault(property) && isDefault(price)) {
+        return house;
+      }
+      // if country is not default
+      if (!isDefault(country) && isDefault(property) && isDefault(price)) {
+        return house.country === country;
+      }
+      // if property is not default
+      if (!isDefault(property) && isDefault(country) && isDefault(price)) {
+        return house.type === property;
+      }
+      // if price is not default
+      if (!isDefault(price) && isDefault(country) && isDefault(property)) {
+        if (housePrice >= minPrice && housePrice <= maxPrice) {
+          return house;
+        }
+      }
+      // if country && property is not default
+      if (!isDefault(country) && !isDefault(property) && isDefault(price)) {
+        return house.country === country && house.type === property;
+      }
+      // if country && price is not default
+      if (!isDefault(country) && isDefault(property) && !isDefault(price)) {
+        if (housePrice >= minPrice && housePrice <= maxPrice) {
+          return house.country === country;
+        }
+      }
+      // if property && price is not default
+      if (isDefault(country) && isDefault(property) && !isDefault(price)) {
+        if (housePrice >= minPrice && housePrice <= maxPrice) {
+          return house.type === property;
+        }
+      }
+      return false;
+    });
+
+    setTimeout(() => {
+      return (
+        newHouses.length < 1 ? setHouses([]) : setHouses(newHouses),
+        setLoading(false)
+      );
+    }, 1000);
+  };
+
+  return (
+    <HouseContext.Provider
+      value={{
+        country,
+        setCountry,
+        countries,
+        property,
+        setProperty,
+        properties,
+        price,
+        setPrice,
+        houses,
+        loading,
+        handleClick,
+      }}
+    >
+      {children}
+    </HouseContext.Provider>
+  );
 };
 
-export default HouseContext;
+export default HouseContextProvider;
